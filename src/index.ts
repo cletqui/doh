9dd6796ad986f5073c8336445b1673a7af0ddd0c
cletqui <antoineq29@gmail.com>
2024-04-30 13:03:16 +0200

    v0.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
 
-// import users from "./endpoints/users";
 import dnsQuery from "./endpoints/dns-query";
 
 const app = new OpenAPIHono();
 
 /* ROUTES */
-// app.route("/users", users);
 app.route("/dns-query", dnsQuery);
 
 /* SWAGGER */
@@ -17,7 +15,7 @@ app.get("/", swaggerUI({ url: "/json" }));
 app.doc("/json", {
   openapi: "3.0.0",
   info: {
-    version: "0.1.0",
+    version: "0.1.1",
     title: "DoH",
   },
 });
